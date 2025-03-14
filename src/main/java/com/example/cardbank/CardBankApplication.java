0bf35f479ec787a6666149b951c3b7cce5197547
Aléxis Toigo <alexistoigo@gmail.com>
2025-03-14 17:49:36 -0300

    properties

--- src/main/java/com/example/cardbank/CardBankApplication.java
+++ src/main/java/com/example/cardbank/CardBankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardBankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardBankApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/cardbank/CardBankApplicationTests.java
+++ src/test/java/com/example/cardbank/CardBankApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CardBankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
